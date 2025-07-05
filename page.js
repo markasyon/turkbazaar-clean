9019d546930f4294ba9f68addeedf91c90327353
markasyon <dmrhsy1997@gmail.com>
2025-07-06 01:31:51 +0300

    Update page.js

--- page.js
+++ page.js
@@ -101,3 +101,11 @@ export default function Home() {
     </div>
   );
 }
+export default function HomePage() {
+  return (
+    <div>
+      <h1>TurkBazaar'a Hoş Geldiniz</h1>
+      <p>Türk üreticilerle dünyayı buluşturuyoruz.</p>
+    </div>
+  );
+}
