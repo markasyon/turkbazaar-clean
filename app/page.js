f27b019ca250af6def88dd025850d49e344bfc2d
markasyon <dmrhsy1997@gmail.com>
2025-07-06 02:51:17 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 import { Button } from "@/components/ui/button";
 
-export default function HomePage() {
+export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center">
-      <Button className="text-white bg-green-600 hover:bg-green-700">
-        TurkBazaar'a Hoş Geldin!
-      </Button>
+    <main className="flex min-h-screen items-center justify-center">
+      <Button>Butona Tıkla</Button>
     </main>
   );
 }
