c4047ef8e9c72986e167ecc2714f88953d402133
markasyon <dmrhsy1997@gmail.com>
2025-07-06 02:37:46 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>TurkBazaar'a Hoş Geldiniz</h1>
-      <p>Türk üreticilerle dünyayı buluşturuyoruz.</p>
-    </div>
+    <main className="flex h-screen items-center justify-center">
+      <Button className="text-white bg-green-600 hover:bg-green-700">
+        TurkBazaar'a Hoş Geldin!
+      </Button>
+    </main>
   );
 }
